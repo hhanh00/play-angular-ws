9b6477947e8027c25bf289e1171c08afd7b0a978
Hanh <hanh425@gmail.com>
2013-11-30 10:52:17 +0800

    websocket

--- app/controllers/AppController.scala
+++ app/controllers/AppController.scala
@@ -5,36 +5,64 @@ import play.api.mvc._
 import play.api.Play.current
 import play.api.libs.concurrent._
 import akka.actor._
+import rx.lang.scala.Observable
+import play.api.libs.iteratee._
+import play.api.libs.iteratee.Concurrent.Channel
 
-class TimerActor extends Actor {
-  def receive = {
-    case _ =>
+class SourceActor extends FSM[SourceActor.State, SourceActor.Data] {
+  import SourceActor._
+  startWith(Disconnected, Unsubscribed)
+
+  when(Disconnected) {
+    case Event(Open(channel), Unsubscribed) =>
+      goto(Connected) using Subscribed(channel)
   }
+
+  when(Connected) {
+    case Event(Close, _: Subscribed) =>
+      goto(Disconnected) using Unsubscribed
+  }
+
+  initialize()
 }
-object TimerActor {
-  case object Start
-  case object Stop
+
+object SourceActor {
+  trait State
+  case object Disconnected extends State
+  case object Connected extends State
+  trait Data
+  case object Unsubscribed extends Data
+  case class Subscribed(channel: Channel[String]) extends Data
+
+  case class Open(channel: Channel[String])
+  case object Close
 }
 
 object AppController extends Controller {
-  import TimerActor._
-  val timerActor = Akka.system.actorOf(Props[TimerActor])
-
+  val system = ActorSystem()
+  implicit val ec = system.dispatcher
+  val sourceActor = system.actorOf(Props[SourceActor])
+  
   def index = Action {
     Ok(views.html.app.index())
   }
 
-  def start = Action { implicit request => 
-    timerActor ! Start
-    Ok("")
-    }
+  def start = Action { implicit request =>
+    val source = Observable(0 until 10000)
 
-  def stop = Action { implicit request => 
-    timerActor ! Stop
     Ok("")
-    }
+  }
 
-  def indexWS = TODO
+  def stop = Action { implicit request =>
+    Ok("")
+  }
+
+  def indexWS = WebSocket.using[String] { request =>
+    val in = Iteratee.consume[String]().map { _ => sourceActor ! SourceActor.Close }
+    val (out, channel) = Concurrent.broadcast[String]
+    sourceActor ! SourceActor.Open(channel)
+    (in, out)
+  }
 
   def jsRoutes = Action { implicit request =>
     Ok(Routes.javascriptRouter("jsRoutes")(routes.javascript.AppController.start, routes.javascript.AppController.stop, routes.javascript.AppController.indexWS))
--- build.sbt
+++ build.sbt
@@ -5,7 +5,9 @@ version := "1.0-SNAPSHOT"
 libraryDependencies ++= Seq(
   jdbc,
   anorm,
-  cache
+  cache,
+  "com.netflix.rxjava" % "rxjava-core" % "0.15.1",
+  "com.netflix.rxjava" % "rxjava-scala" % "0.15.1"
 )     
 
 play.Project.playScalaSettings
